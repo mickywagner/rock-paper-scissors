f90b7ff3f93a3c324b0c951a79fc692bf6cb956c
Michaela Wagner <mickywagner@gmail.com>
2019-07-16 13:53:29 -0600

    Separates game functions into smaller functions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# rock-paper-scissors
+# rock-paper-scissors - Created for Odin Project Web Development Course
+
+<ol>
+<li>Lanuguages: HTML, CSS, JavaScript</li>
+<li>Original Script uses JavaScript only. Game is played only through browser prompts.</li>
+<li>Final version adds front-end user interface</li>
+<ol>
+
+<img src="GameInterface.png">
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let compScoreCount = document.querySelector('#compScore');
 let playScoreCount = document.querySelector('#playerScore')
 
 function computerPlay() {
+    removeBorder()
     let randomPRS = Math.floor(Math.random() * 3) + 1;
     if (randomPRS == 1) {
         return 'rock';
@@ -14,62 +15,66 @@ function computerPlay() {
     }
 }
 
+function removeBorder() {
+    const compList = document.querySelectorAll('.comp')
+    compList.forEach(li => li.classList.remove('border'))
+}
 
-// Try start game with button click
-// const buttons = document.querySelectorAll('.choice');
-// buttons.addEventListener('click', playRound)
-
-function playRound(playerSelection, computerSelection) {
+const buttons = document.querySelectorAll('.choice');
+buttons.forEach(button => button.addEventListener('click', function(e){
+    let playerSelection = e.target.closest('button').id
+    
+    let computerSelection = computerPlay()
     let rockBorder = document.querySelector('#compR')
     let paperBorder = document.querySelector('#compP')
     let scissorsBorder = document.querySelector('#compS')
 
-    let buttons = document.querySelectorAll('.choice');
-    buttons.forEach(button => button.addEventListener('click', function (e) {
-        playerSelection = e.target.id 
-        computerSelection = computerPlay()
-        
-        if(computerSelection === 'rock'){
-            rockBorder.classList.add('border')
-        } else if(computerSelection === 'paper') {
-            paperBorder.classList.add('border')
-        } else {
-            scissorsBorder.classList.add('border')
-        }
+    if(computerSelection === 'rock'){
+        rockBorder.classList.add('border')
+    } else if(computerSelection === 'paper') {
+        paperBorder.classList.add('border')
+    } else {
+        scissorsBorder.classList.add('border')
+    }
 
-        if (computerSelection === 'rock' && playerSelection === 'scissors') {
-            computerScore++;
-        } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-            computerScore++;
-        } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-            computerScore++;
-        } else if (computerSelection === playerSelection) {
-        } else {
-            playerScore++;
-        }
+    if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        computerScore++;
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        computerScore++;
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        computerScore++;
+    } else if (computerSelection === playerSelection) {
+    } else {
+        playerScore++;
+    }
 
-        playScoreCount.textContent = "Player Score: " + playerScore
-        compScoreCount.textContent = "Computer Score: " + computerScore
+    playScoreCount.textContent = "Player Score: " + playerScore
+    compScoreCount.textContent = "Computer Score: " + computerScore
 
-        if (playerScore === 5) {
-            alert("You win! You beat the computer " + playerScore + " to " + computerScore + "!")
-            resetGame()
-        } else if(computerScore === 5) {
-            alert("You lost! The computer beat you " + computerScore + " to " + playerScore + ". Better luck next time!")
-            resetGame()
-        }
-    }))
+    checkScore()
+}))
+
+function checkScore() {
+    if (playerScore < 5 && computerScore < 5) {
+        return
+    } else if (playerScore === 5) {
+        alert("You win! You beat the computer " + playerScore + " to " + computerScore + "!")
+        resetGame()
+    } else {
+        alert("You lost! The computer beat you " + computerScore + " to " + playerScore + ". Better luck next time!")
+        resetGame()
+    } 
 }
 
 function resetGame() {
+    removeBorder()
     computerScore = 0;
     playerScore = 0;
     compScoreCount.textContent = "Computer Score: " + computerScore
     playScoreCount.textContent = "Player Score: " + playerScore
-    playRound()
 }
 
 const startBtn = document.querySelector('#start')
 startBtn.addEventListener('click', resetGame)
 
-playRound()
+
